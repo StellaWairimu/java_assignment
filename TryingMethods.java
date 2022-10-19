0624a3febf46e41e83ad6cdb8571b9a2d7611453
stella <stellawaihuini@gmail.com>
2022-10-19 08:35:42 +0000

    Trying methods file

---
+++ TryingMethods.java
@@ -0,0 +1,99 @@
+//Main Class
+    /*This script runs us through the various java methods , dynamic and static.
+     * We shall be practising the use of switch cases and the if statements in both dynamic and
+     * static methods.
+     */
+public class TryingMethods {
+
+        //Method 1
+     public static void main(String[] args) {
+            iftest(10);
+            
+            
+           TryingMethods obj = new TryingMethods();
+           
+            //This method return somthing
+             String day = obj.daysOfWeek(10);   
+             
+             System.out.println(day);
+
+            sumofNumbers();
+            evenNumbers(); 
+        }
+
+     //Method 2
+        public static void iftest (int y ) {
+
+
+        if (y < 2) {
+            System.out.println("Minimum wage grade");
+           }
+
+           else if (y > 1) {
+            System.out.println("Middle Income earner");
+           }
+
+           else if (y > 2) {
+            System.out.println("Top Earner");
+           }
+
+           else
+           {
+            System.out.println("Cannot be graded, enter a valid wage Grade .Between 1 - 3");
+           }
+
+         
+     }
+      public String  daysOfWeek (int day){
+                
+        switch(day) {
+            case 1:
+            return "Today is Monday" ;
+            
+            case 2:
+            return "Today is Tuesday" ;
+
+            case 3:
+            return "Today is Wednesday" ;
+            
+            case 4:
+            return "Today is Thursday" ;
+
+            case 5:
+            return "Today is Friday" ;
+
+            default:
+            return "Not a weekday , enter days between 1 to 5";
+
+
+        }
+
+        
+      }
+      //Sum of numbers <1000 
+
+      public static int sumofNumbers( ) {
+        int sum = 0, i=1;
+        while (i<1000); {
+            sum+=i;
+            i++;
+        }
+        return sum;
+      }
+
+      public static int evenNumbers() {
+        int sum = 0;
+        for (int x= 12; x<=103; x++) 
+        {
+          if (x % 2 == 0 );
+          {
+            sum +=x ;
+            
+          }
+          
+        }
+        return sum;
+      }
+        
+       
+}
